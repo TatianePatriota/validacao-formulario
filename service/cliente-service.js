0e882c8714fd64845121ce2fcf7ac7791715212e
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-09-09 10:30:21 -0300

    em andament0

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,42 +1,10 @@
-const criaLinha = (nome, email) => {
-  const linhaNovoCliente = document.createElement('tr');
-  const conteudo = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>`
+const listaClientes = () => {
+  return fetch(`http://localhost:3000/profile`)
+  .then(resposta => {
+    return resposta.json()
+  })
+};
 
-  linhaNovoCliente.innerHTML = conteudo;
-  return linhaNovoCliente;
-}
-
-const tabela = document.querySelector('[data-tabela]');
-
-const http = new XMLHttpRequest();
-http.open('GET', 'http://localhost:3000/profile');
-http.send();
-
-http.onload = () => {
-  const data = JSON.parse(http.response);
-  
-  data.forEach(elemento => {
-    tabela.appendChild(criaLinha(elemento.nome, elemento.email))
-  });
-
-  const http2 = new XMLHttpRequest();
-  http2.open('GET', 'http://localhost:3000/profile/semanaAnterior');
-
-  http2.onload = () => {
-    const data = JSON.parse(http.response);
-    data.forEach(elemento => {
-    tabela.appendChild(criaLinha(elemento.nome, elemento.email))
-  });
-  http2.send();
-  }
-
-
+export const clienteService = {
+  listaClientes
 }
